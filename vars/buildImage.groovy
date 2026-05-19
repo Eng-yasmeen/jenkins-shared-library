c89ad9169438131963f886d9539a5fcf8b6ffbcf
Ubuntu <ubuntu@ip-172-31-5-207.eu-north-1.compute.internal>
2026-05-19 18:12:44 +0000

    Update buildImage with push logic

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,4 +1,15 @@
-def call() {
-    echo "Image building..."
-    sh 'docker build -t my-app:${BUILD_NUMBER} .'
+def call(String registryUrl, String credentialsId, String imageName) {
+    echo " Start building..."
+     
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        sh "echo $PASS | docker login $registryUrl -u $USER --password-stdin"
+        
+
+        sh "docker build -t $registryUrl/$imageName:${BUILD_NUMBER} ."
+        
+     
+        sh "docker push $registryUrl/$imageName:${BUILD_NUMBER}"
+    }
+    
+    echo "Image uploaded successfullu: $registryUrl/$imageName:${BUILD_NUMBER}"
 }
